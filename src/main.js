af3a44b9d8247356c0a2baec485ea80362ca25c8
Teisha <colorfulspirit00@gmail.com>
2024-01-31 20:06:19 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import "./all.scss"
+
+const fun = () => {
+  const message = () => {
+    return 'Hello, Babel!';
+  };
+  
+  console.log(message());
+}
+fun()
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+module.exports = {
+  context: path.resolve(__dirname, './src'),
+  entry: './main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(js)$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ],
+  },
+};
