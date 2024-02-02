7fda61e00efb52835094ad9a5e0de4364efc32b4
Teisha <colorfulspirit00@gmail.com>
2024-02-02 10:58:08 +0800

    one

--- src/components/CustomInputNumber/index.js
+++ src/components/CustomInputNumber/index.js
@@ -1,35 +1,115 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useMemo, useEffect } from "react";
 
 const CustomInputNumber = (props) => {
-  const { min, max, step, name, value, disabled, onChange, onBlur } = props;
-  const [number, setNumber] = useState(0);
-  const handleInputChange = (event) => {
-    setNumber(event.target.value)
-  }
-  const handleChange = (type) => {
-    
+  const { min = 0, max, step = 1, name, value, disabled, onChange, onBlur } = props;
+  const [inputNumber, setInputNumber] = useState(value || 0);
+  const [intervalId, setIntervalId] = useState(null);
+  const [isMinDisabled, setIsMinDisabled] = useState(false);
+  const [isMaxDisabled, setIsMaxDisabled] = useState(false);
+  const hasBlurredRef = useRef(false);
 
+  const minDisabled = useMemo(() => value <= min || disabled, [value, min, disabled])
+  const maxDisabled = useMemo(() => value >= max || disabled, [value, max, disabled])
+
+
+  useEffect(() => {
+    if (minDisabled || maxDisabled) {
+      stopAccumulating()
+    }
+  }, [minDisabled, maxDisabled]);
+
+  const handleChange = (event, type) => {
+    setInputNumber(pre => {
+      let updateNum = 0;
+      if (type === "input") updateNum = parseInt(event.target.value, 10) || 0;
+      if (type === "plus") updateNum = Number(pre) + step;
+      if (type === "minus") updateNum = Number(pre) - step;
+      // number over setting
+      console.log(type, updateNum);
+      if (updateNum < min || updateNum > max) {
+        updateNum = updateNum > max ? pre : min;
+        event.target.value = updateNum;
+        event.target.name = name;
+        stopAccumulating()
+        if (!hasBlurredRef.current) {
+          handleOnBlur(event, updateNum);
+          hasBlurredRef.current = true;
+        }
+
+      } else {
+        event.target.value = updateNum;
+        event.target.name = name;
+        onChange(event)
+      }
+
+      // stop setInterval if already min or max
+      // setIsMinDisabled(updateNum <= min);
+      // setIsMaxDisabled(updateNum >= max);
+
+
+      if (updateNum <= min || updateNum >= max) {
+        stopAccumulating()
+        // setIsMinDisabled(true);
+      }
+      // if (updateNum === max) {
+      //   handleMouseUp()
+      //   setIsMaxDisabled(true);
+      // }
+
+      return updateNum;
+    })
   }
+
+  const handleMouseDown = (event, type) => {
+    handleChange(event, type); // click once
+    //多個判斷:按太久開始累加
+    const id = setInterval(() => handleChange(event, type), 300);
+    setIntervalId(id);
+  }
+
+  const stopAccumulating = () => {//停止累加
+    if (intervalId !== null) {
+      clearInterval(intervalId);
+      setIntervalId(null);
+      return
+    }
+  }
+
+  const handleOnBlur = (event, updateNum) => {
+    stopAccumulating()
+    event.target.name = name;
+    event.target.value = updateNum || Number(inputNumber);
+    console.log("handleOnBlur", { name, updateNum, inputNumber }, updateNum || Number(inputNumber));
+    onBlur(event)
+  }
+
   return (
-    <div className="inputBox">
+    <div className="inputBox" onBlur={() => handleOnBlur({ target: { name, value: inputNumber } })}>
       <button
         type="button"
-        disabled={disabled}
-        onClick={() => handleChange("minus")}
+        disabled={minDisabled}
+        onMouseDown={(event) => handleMouseDown(event, "minus")}
+        onMouseUp={stopAccumulating}
+        // onMouseLeave={handleMouseUp}
+        className={disabled ? "disabled" : ""}
       >
         <i className="fa fa-solid fa-minus"></i>
       </button>
       <input
         type="number"
-        value={number}
-        min={min || 0}
+        name={name}
+        value={inputNumber}
+        min={min}
         max={max}
-        onChange={handleInputChange}
+        onChange={event => handleChange(event, "input")}
       />
       <button
         type="button"
-        disabled={disabled}
-        onClick={() => handleChange("plus")}
+        disabled={maxDisabled}
+        onMouseDown={(event) => handleMouseDown(event, "plus")}
+        onMouseUp={stopAccumulating}
+        // onMouseLeave={handleMouseUp}
+        className={disabled ? "disabled" : ""}
       >
         <i className="fa fa-solid fa-plus"></i>
       </button>
--- src/components/RoomAllocation/index.js
+++ src/components/RoomAllocation/index.js
@@ -1,20 +1,95 @@
-import React from "react";
-import Room from "./../Room";
+import React, { useEffect, useState } from "react";
+import CustomInputNumber from "./../CustomInputNumber"
 
 const RoomAllocation = (props) => {
   const { guest, room, onChange } = props;
-  const roomArray = Array.from({ length: room }, (_, index) => index + 1);
-  const aaa = [{
-    adult: 1, child: 0
-  }, { adult: 1, child: 0 }, { adult: 1, child: 0 }]
+  const [orderData, setOrderData] = useState([])
+  const [remainGuest, setRemainGuest] = useState(guest)
+
+  useEffect(() => {
+    const defaultOrder = Array.from({ length: room }, (_, index) => ({
+      adult: 1,
+      child: 0,
+      ui_id: `room${index}`
+    }));
+    const defaultAdultNum = defaultOrder.reduce((sum, item) => sum + item.adult, 0)
+    setRemainGuest(guest - defaultAdultNum)
+    setOrderData(defaultOrder)
+  }, [])
+
+  const handleChangeOrder = (event, ui_id) => { // New value
+    const value = event.target.value;
+    const name = event.target.name;
+    // console.log("New value:", { value, name, ui_id },orderData)
+    // You can add logic here if needed
+    // For now, no specific logic is applied
+    return
+  }
+  const handleOnBlur = (event, ui_id) => { // Final value
+    const value = event.target.value;
+    const name = event.target.name;
+
+    setOrderData(preOrderData => {
+      const arrayIdx = preOrderData.findIndex(el => el.ui_id === ui_id)
+      const updateData = JSON.parse(JSON.stringify(preOrderData))
+      updateData[arrayIdx][name] = Number(value);
+
+      // calculate the remaining guests for the current room
+      calculateRemainGuest(updateData)
+
+      // update
+      if (onChange) onChange(updateData);
+      return updateData
+    })
+  }
+
+  const calculateRemainGuest = (updateData) => {
+    const currentPeople = updateData.reduce((sum, item) => sum + item.child + item.adult, 0)
+    setRemainGuest(guest - currentPeople)
+  }
 
   return (
     <div style={{ width: '350px' }}>
       <div className="title">住客人數: {guest}人/{room}房</div>
-      <div className="reminderMessage">尚未分配人數: 7人</div>
-      {roomArray.map(el => (
-        <Room key={`room${el}`} />
-      ))}
+      <div className="reminderMessage">尚未分配人數：{remainGuest}人</div>
+      {orderData.map(el => {
+        return (
+          <div className="roomWrapper" key={el.ui_id}>
+            <div className="title">房間：{Number(el.adult) + Number(el.child)}人</div>
+            <div className="itemWrapper">
+              <div className="itemLabel">
+                大人
+                <div>年齡 20+</div>
+              </div>
+              <CustomInputNumber
+                min={1}
+                max={Math.min(4, remainGuest)}
+                step={1}
+                name="adult"
+                value={el.adult}
+                disabled={false}//人數滿
+                onChange={event => handleChangeOrder(event, el.ui_id)}
+                onBlur={(event) => handleOnBlur(event, el.ui_id)}
+              />
+            </div>
+            <div className="itemWrapper">
+              <div className="itemLabel">
+                小孩
+              </div>
+              <CustomInputNumber
+                min={0}
+                max={Math.min(4, remainGuest)}
+                step={1}
+                name="child"
+                value={el.child}
+                disabled={false}
+                onChange={event => handleChangeOrder(event, el.ui_id)}
+                onBlur={(event) => handleOnBlur(event, el.ui_id)}
+              />
+            </div>
+          </div>
+        )
+      })}
     </div>
   )
 }
